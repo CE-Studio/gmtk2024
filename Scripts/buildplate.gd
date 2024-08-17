46776453f51459b3ee9c794a28c061c251d657f0
clarence112 <clarencepacelli@gmail.com>
2024-08-17 00:57:26 -0400

    a

--- Scripts/buildplate.gd
+++ Scripts/buildplate.gd
@@ -5,6 +5,29 @@ class_name BuildPlate
 static var _area:Array = []
 
 
+static func getpos(pos:Vector3i) -> bool:
+	if (pos.x > 24) or (pos.x < 0):
+		return true
+	if (pos.y > 49) or (pos.x < 0):
+		return true
+	if (pos.z > 24) or (pos.x < 0):
+		return true
+	return _area[pos.x][pos.y][pos.z]
+
+
+static func posConvert(pos:Vector3) -> Vector3i:
+	var x := floori(remap(pos.x, -1, 1, 0, 25))
+	var y := floori(remap(pos.y, 0, 1, 0, 25))
+	var z := floori(remap(pos.z, -1, 1, 0, 25))
+	return Vector3i(x, y, z)
+
+
+static func unConvert(pos:Vector3i) -> Vector3:
+	var x := remap(pos.x, 0, 25, -1, 1)
+	var y := remap(pos.y, 0, 25, 0, 1)
+	var z := remap(pos.z, 0, 25, -1, 1)
+	return Vector3(x, y, z)
+
 func _ready() -> void:
 	for x in 25:
 		_area.append([])
@@ -15,4 +38,11 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	pass
+	rotate_y(Input.get_axis("game_rotate_ccw", "game_rotate_cw") * _delta)
+
+
+func click_event(_camera: Node, event: InputEvent, pos: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event.is_action_pressed("game_place"):
+		var a = posConvert(to_local(pos + Vector3(0, 0.001, 0)))
+		print(a)
+		$MeshInstance3D2.position = unConvert(a)
