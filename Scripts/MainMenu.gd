3ecad0d5eced51a9610cc114a398878207c641a2
EpsilonTheDerg <jamesclamey@gmail.com>
2024-08-18 01:44:14 -0400

    Menu progress

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,11 +1,33 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+const fade_amount:float = 15.0
+var credits_open:bool = false
+
+
+@onready var credits:TextureRect = $"CreditsParent"
+
+
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if InputEventMouseButton and credits_open:
+		credits_open = false
+	if credits_open:
+		credits.modulate.a = lerp(0.0, 1.0, credits.modulate.a + (delta * fade_amount))
+	else:
+		credits.modulate.a = lerp(0.0, 1.0, credits.modulate.a - (delta * fade_amount))
+
+
+func on_play_button():
+	print("Play!")
+	if !credits_open:
+		pass
+
+
+func on_credits_button():
+	print("Credits!")
+	credits_open = true
+
